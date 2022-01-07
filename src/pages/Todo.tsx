05cdcc0a6c54f873ee6ceba6a161854892e73bf2
Kei Murakami <buncho514@icloud.com>
2022-01-07 22:24:16 +0900

    プリミティブなところができた

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,15 +1,26 @@
-function Todo() {
+import React, { useState } from "react";
+
+const Todo = () => {
+  const [wantToBuy, setWantToBuy] = useState<string>("");
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    setWantToBuy(event.target[0].value);
+  };
+
   return (
     <div className="Todo">
       ここにTODO LISTを作るぞ.
-      <form>
-        <input type="text" name="wantToBuy" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
         <button type="submit">TODO追加</button>
       </form>
       <hr></hr>
       <div>ここにリストを表示</div>
+      {wantToBuy}
     </div>
   );
-}
+};
+
 // これは追加分です
 export default Todo;
