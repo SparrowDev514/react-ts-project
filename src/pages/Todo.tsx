cd0ff4c92514a313a619d7d52f0d88ff9d08a389
Kei Murakami <buncho514@icloud.com>
2022-01-08 18:43:10 +0900

    TODOリストができた(型はどうにかしたい)

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,26 +1,37 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Todo = () => {
-  const [wantToBuy, setWantToBuy] = useState<string>("");
+  const [idCounter, setIdCounter] = useState<number>(0);
+  const [wantToBuys, setWantToBuy] = useState<Object[]>([]);
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    setWantToBuy(event.target[0].value);
+  /**
+   * フォーム送信したらtodo配列にtodoを追加
+   * @param {Event} e 送信イベント
+   */
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const inputText: String = e.target["wantToBuy"].value;
+    const nextid: number = idCounter + 1;
+    setIdCounter(nextid);
+    setWantToBuy([...wantToBuys, { id: nextid, item: inputText }]);
   };
 
   return (
     <div className="Todo">
       ここにTODO LISTを作るぞ.
       <form onSubmit={handleSubmit}>
-        <input type="text" />
-        <button type="submit">TODO追加</button>
+        <input name="wantToBuy" />
+        <button>TODO追加</button>
       </form>
       <hr></hr>
       <div>ここにリストを表示</div>
-      {wantToBuy}
+      <div>
+        {wantToBuys.map((wantToBuy: any) => (
+          <li key={wantToBuy.id}>{wantToBuy.item}</li>
+        ))}
+      </div>
     </div>
   );
 };
-
 // これは追加分です
 export default Todo;
