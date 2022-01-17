b01a6038d3fe782dcacf98d3dd040922fca63dec
Kei Murakami <buncho514@icloud.com>
2022-01-17 21:14:48 +0900

    工事中ページ導入

---
+++ src/pages/CommitCounter.tsx
@@ -0,0 +1,5 @@
+const CommitCounter = () => {
+  return <div className="CommitCounter">工事中</div>;
+};
+
+export default CommitCounter;
---
+++ src/pages/FakeTwitter.tsx
@@ -0,0 +1,5 @@
+const FakeTwitter = () => {
+  return <div className="FakeTwitter">工事中</div>;
+};
+
+export default FakeTwitter;
