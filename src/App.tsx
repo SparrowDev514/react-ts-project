b3670a612eceb48c9b553dc43cfb288cecd4501b
Kei Murakami <buncho514@icloud.com>
2022-01-23 19:25:46 +0900

    ルーターを分離

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
 import "./App.css";
-import { VFC } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import WantToBuy from "./pages/WantToBuy";
-import CommitCounter from "./pages/CommitCounter";
-import FakeTwitter from "./pages/FakeTwitter";
+import Nav from "./router/Nav";
 
-const App: VFC = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            {/* <li>
-              <Link to="/">Home</Link>
-            </li> */}
-            <li>
-              <Link to="/wantToBuy">WantToBuy</Link>
-            </li>
-            <li>
-              <Link to="/commitCounter">CommitCounter</Link>
-            </li>
-            <li>
-              <Link to="/fakeTwitter">FakeTwitter</Link>
-            </li>
-          </ul>
-        </nav>
-        <hr />
-        <Routes>
-          <Route path="/wantToBuy" element={<WantToBuy />} />
-          <Route path="/commitCounter" element={<CommitCounter />} />
-          <Route path="/fakeTwitter" element={<FakeTwitter />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+const App = () => {
+  return <Nav />;
 };
 
 export default App;
