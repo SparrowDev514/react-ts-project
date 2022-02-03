c4b804b04eb4accd65d23e1ef443dca5373e5f66
Kei Murakami <buncho514@icloud.com>
2022-02-03 21:59:40 +0900

    ライフゲーム作る

---
+++ src/pages/LifeGame.tsx
@@ -0,0 +1,5 @@
+const LifeGame = () => {
+  return <div className="LifeGame">工事中</div>;
+};
+
+export default LifeGame;
--- src/router/Nav.tsx
+++ src/router/Nav.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import WantToBuy from "../pages/WantToBuy";
 import CommitCounter from "../pages/CommitCounter";
 import FakeTwitter from "../pages/FakeTwitter";
+import LifeGame from "../pages/LifeGame";
 const Nav: VFC = () => {
   return (
     <BrowserRouter>
@@ -22,6 +23,9 @@ const Nav: VFC = () => {
             <li>
               <Link to="/fakeTwitter">FakeTwitter</Link>
             </li>
+            <li>
+              <Link to="/lifeGame">LifeGame</Link>
+            </li>
           </ul>
         </nav>
         <hr />
@@ -29,6 +33,7 @@ const Nav: VFC = () => {
           <Route path="/wantToBuy" element={<WantToBuy />} />
           <Route path="/commitCounter" element={<CommitCounter />} />
           <Route path="/fakeTwitter" element={<FakeTwitter />} />
+          <Route path="/lifeGame" element={<LifeGame />} />
         </Routes>
       </div>
     </BrowserRouter>
