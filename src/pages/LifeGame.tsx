2781077d252174b4ebdb0d855ba98e06222446ab
Kei Murakami <buncho514@icloud.com>
2022-02-11 22:54:42 +0900

    ライフゲームの板を表示

--- src/pages/LifeGame.tsx
+++ src/pages/LifeGame.tsx
@@ -1,5 +1,25 @@
 const LifeGame = () => {
-  return <div className="LifeGame">工事中</div>;
+  let square: object[] = [];
+  let div: object[] = [];
+  let horizonalNum: number = 20;
+  let verticalNum: number = 20;
+
+  const createLifeGameBoard = () => {
+    for (let i = 0; i < verticalNum; i++) {
+      // forで回すとき都度都度配列初期化する
+      square = [];
+      for (let j = 0; j < horizonalNum; j++) {
+        square.push(<button className="Square" key={j} />);
+      }
+      div.push(
+        <div className="SquareRow" key={i}>
+          {square}
+        </div>
+      );
+    }
+    return div;
+  };
+  return <div className="LifeGame">{createLifeGameBoard()}</div>;
 };
 
 export default LifeGame;
