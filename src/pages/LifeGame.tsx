85a0ec1bc7249e6fb0eae26eba538e782055e11b
Kei Murakami <buncho514@icloud.com>
2022-02-11 23:40:01 +0900

    はい

--- src/pages/LifeGame.tsx
+++ src/pages/LifeGame.tsx
@@ -4,12 +4,26 @@ const LifeGame = () => {
   let horizonalNum: number = 20;
   let verticalNum: number = 20;
 
+  // ライフゲームの状況を保持する
+  const lifeGameState = () => {};
+
+  // ライフゲームの状況によってマス目ごとの状況(◉ or "")を返す
+  const setLifeGameState = (horizonal: number, vertical: number) => {
+    return "(" + horizonal + "," + vertical + ")";
+  };
+
+  // ライフゲームの板を描写する
   const createLifeGameBoard = () => {
     for (let i = 0; i < verticalNum; i++) {
       // forで回すとき都度都度配列初期化する
       square = [];
       for (let j = 0; j < horizonalNum; j++) {
-        square.push(<button className="Square" key={j} />);
+        // lifeGameStateによって
+        square.push(
+          <button className="Square" key={j}>
+            {setLifeGameState(j, i)}
+          </button>
+        );
       }
       div.push(
         <div className="SquareRow" key={i}>
