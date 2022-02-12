8046266357aa09fc19e8c25c26147925f1b0cf2f
Kei Murakami <buncho514@icloud.com>
2022-02-12 23:47:51 +0900

    枠組みだけ

--- src/pages/LifeGame.tsx
+++ src/pages/LifeGame.tsx
@@ -2,15 +2,36 @@ const LifeGame = () => {
   let square: object[] = [];
   let div: object[] = [];
   let horizonalNum: number = 20;
-  let verticalNum: number = 20;
+  let verticalNum: number = 10;
 
-  // ライフゲームの状況を保持する
-  const lifeGameState = () => {};
+  // ライフゲームの状況を保持する定数
+  let LifeGameState: number[][] = [];
 
-  // ライフゲームの状況によってマス目ごとの状況(◉ or "")を返す
-  const setLifeGameState = (horizonal: number, vertical: number) => {
-    return "(" + horizonal + "," + vertical + ")";
+  //最初に発生させる生命の割合
+  const initialRate: number = 0.5;
+
+  // LifeGameStateを乱数で初期化
+  const initializeState = () => {
+    for (let i = 0; i < verticalNum; i++) {
+      let row: number[] = [];
+      for (let j = 0; j < horizonalNum; j++) {
+        row.push(Math.random());
+      }
+      LifeGameState.push(row);
+    }
+    return LifeGameState;
   };
+
+  // 配列の状況に応じて値を返す
+  const returnState = (i: number, j: number) => {
+    if (LifeGameState[i][j] > initialRate) {
+      return "";
+    } else {
+      return "●";
+    }
+  };
+
+  initializeState();
 
   // ライフゲームの板を描写する
   const createLifeGameBoard = () => {
@@ -18,10 +39,9 @@ const LifeGame = () => {
       // forで回すとき都度都度配列初期化する
       square = [];
       for (let j = 0; j < horizonalNum; j++) {
-        // lifeGameStateによって
         square.push(
           <button className="Square" key={j}>
-            {setLifeGameState(j, i)}
+            {returnState(i, j)}
           </button>
         );
       }
