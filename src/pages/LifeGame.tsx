38c521a880eb8794ec2685f4e03ab14a874a0372
Kei Murakami <buncho514@icloud.com>
2022-02-13 13:44:23 +0900

    八近傍の状況まとめ

--- src/pages/LifeGame.tsx
+++ src/pages/LifeGame.tsx
@@ -5,7 +5,7 @@ const LifeGame = () => {
   let verticalNum: number = 10;
 
   // ライフゲームの状況を保持する定数
-  let LifeGameState: number[][] = [];
+  let lifeGameState: string[][] = [];
 
   //最初に発生させる生命の割合
   const initialRate: number = 0.5;
@@ -13,38 +13,65 @@ const LifeGame = () => {
   //step
   let step: number = 0;
 
-  // LifeGameStateを乱数で初期化
+  // lifeGameStateを乱数で初期化
   const initializeState = () => {
     for (let i = 0; i < verticalNum; i++) {
-      let row: number[] = [];
+      let row: string[] = [];
       for (let j = 0; j < horizonalNum; j++) {
-        row.push(Math.random());
+        if (Math.random() > initialRate) {
+          row.push("");
+        } else {
+          row.push("●");
+        }
       }
-      LifeGameState.push(row);
+      lifeGameState.push(row);
     }
-    return LifeGameState;
+    return lifeGameState;
+  };
+
+  //世代を進める
+  const nextState = () => {
+    step = step + 1;
+    createLifeGameBoard(step);
   };
 
   // 配列の状況に応じて値を返す
   const returnState = (i: number, j: number, step: number) => {
     if (step == 0) {
-      if (LifeGameState[i][j] > initialRate) {
-        return "";
-      } else {
-        return "●";
-      }
+      return lifeGameState[i][j];
     } else {
       //１世代以降の処理
+      // 八近傍の状況
+      let upperLeft = i == 0 || j == 0 ? "" : lifeGameState[i - 1][j - 1];
+      let upper = i == 0 ? "" : lifeGameState[i - 1][j];
+      let upperRight =
+        i == 0 || j == horizonalNum - 1 ? "" : lifeGameState[i - 1][j + 1];
+      let left = j == 0 ? "" : lifeGameState[i][j - 1];
+      let right = j == horizonalNum - 1 ? "" : lifeGameState[i][j + 1];
+      let lowerLeft =
+        i == verticalNum - 1 || j == 0 ? "" : lifeGameState[i + 1][j - 1];
+      let lower = i == verticalNum - 1 ? "" : lifeGameState[i + 1][j];
+      let lowerRight =
+        i == verticalNum - 1 || j == horizonalNum - 1
+          ? ""
+          : lifeGameState[i + 1][j + 1];
+      // 八近傍の状況をまとめた配列
+      let neighborhoods = [
+        upperLeft,
+        upper,
+        upperRight,
+        left,
+        right,
+        lowerLeft,
+        lower,
+        lowerRight,
+      ];
+      console.log(neighborhoods);
     }
   };
 
-  //世代を進める
-  const nextState = () => {};
-
-  initializeState();
-
   // ライフゲームの板を描写する
-  const createLifeGameBoard = () => {
+  const createLifeGameBoard = (step: number) => {
     for (let i = 0; i < verticalNum; i++) {
       // forで回すとき都度都度配列初期化する
       square = [];
@@ -63,7 +90,22 @@ const LifeGame = () => {
     }
     return div;
   };
-  return <div className="LifeGame">{createLifeGameBoard()}</div>;
+
+  // 初期化
+  initializeState();
+  return (
+    <div className="lifeGame">
+      <div className="lifeGameBoard">{createLifeGameBoard(step)}</div>
+      <div>
+        <button className="nextStepButton" onClick={nextState}>
+          進める
+        </button>
+      </div>
+      <div>
+        <button className="autoStep">オート</button>
+      </div>
+    </div>
+  );
 };
 
 export default LifeGame;
