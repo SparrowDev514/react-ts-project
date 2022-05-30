02761839fc0874a627f04d725d46e1134177257a
Kei Murakami <buncho514@icloud.com>
2022-05-30 23:14:17 +0900

    stateわからん

--- src/pages/LifeGame.tsx
+++ src/pages/LifeGame.tsx
@@ -1,10 +1,72 @@
+// import
 import React from "react";
 import { useState } from "react";
 
+// state
 const [step, setStep] = useState<number>(0);
+const [rowsNum, setRowsNum] = useState<number>(20);
 
-function sleep(waitTime: number | undefined){
-  new Promise((resolve) => setTimeout(resolve, waitTime))
+// グローバル変数
+let lifeGameState: string[][] = [];
+const nextLifeGameState: string[][] = [];
+
+//　ロジック
+function returnState(i: number, j: number, step: number) {
+  if (step == 0) {
+    return lifeGameState[i][j];
+  } else {
+    //１世代以降の処理
+    // 八近傍の状況
+    const upperLeft = i == 0 || j == 0 ? "" : lifeGameState[i - 1][j - 1];
+    const upper = i == 0 ? "" : lifeGameState[i - 1][j];
+    const upperRight =
+      i == 0 || j == rowsNum - 1 ? "" : lifeGameState[i - 1][j + 1];
+    const left = j == 0 ? "" : lifeGameState[i][j - 1];
+    const right = j == rowsNum - 1 ? "" : lifeGameState[i][j + 1];
+    const lowerLeft =
+      i == rowsNum - 1 || j == 0 ? "" : lifeGameState[i + 1][j - 1];
+    const lower = i == rowsNum - 1 ? "" : lifeGameState[i + 1][j];
+    const lowerRight =
+      i == rowsNum - 1 || j == rowsNum - 1 ? "" : lifeGameState[i + 1][j + 1];
+    // 八近傍の状況をまとめた配列
+    const neighborhoods = [
+      upperLeft,
+      upper,
+      upperRight,
+      left,
+      right,
+      lowerLeft,
+      lower,
+      lowerRight,
+    ];
+    const liveCellNum: number = neighborhoods.filter(
+      (neighborhood) => neighborhood == "●"
+    ).length;
+
+    if (lifeGameState[i][j] == "" && liveCellNum == 3) {
+      nextLifeGameState[i][j] = "●";
+      return "●";
+    } else if (lifeGameState[i][j] == "●") {
+      if (liveCellNum == 2 || liveCellNum == 3) {
+        nextLifeGameState[i][j] = "●";
+        return "●";
+      }
+      if (liveCellNum <= 1 || liveCellNum >= 4) {
+        nextLifeGameState[i][j] = "";
+        return "";
+      }
+    } else {
+      return nextLifeGameState[i][j];
+    }
+  }
+}
+
+function handleRowsNumChange(e: any) {
+  setRowsNum(() => e.target.value);
+}
+
+function sleep(waitTime: number | undefined) {
+  new Promise((resolve) => setTimeout(resolve, waitTime));
 }
 
 async function auto() {
@@ -12,87 +74,123 @@ async function auto() {
     await sleep(100);
     setStep(i);
   }
-};
+}
 
-const nextState = () => {
+function nextState() {
   const nextStep: number = step + 1;
   setStep(nextStep);
-};
+}
 
-class createLifeGameBoard extends React.Component {
-  render(){
-    return()
+function createLifeGameBoard(step: number) {
+  // forで回すとき都度都度配列初期化する
+  const row = [];
+  for (let i = 0; i < rowsNum; i++) {
+    // forで回すとき都度都度配列初期化する
+    const square = [];
+    for (let j = 0; j < rowsNum; j++) {
+      square.push(
+        <button className="Square" key={j}>
+          {returnState(i, j, step)}
+        </button>
+      );
+    }
+    row.push(
+      <div className="SquareRow" key={i}>
+        {square}
+      </div>
+    );
+  }
+  if (step != 0) {
+    lifeGameState = nextLifeGameState;
+  }
+  return row;
+}
+
+// コンポーネント
+class CreateLifeGameBoard extends React.Component {
+  render() {
+    return (
+      <div className="lifeGameBoard" style={lifeGameBoardStyle}>
+        {createLifeGameBoard(this.state.step)}
+      </div>
+    );
   }
 }
 
-class stepNum extends React.Component {
-  render(){
-    return()
+class StepNum extends React.Component {
+  render() {
+    return (
+      <div className="stepNum" style={stepNumStyle}>
+        第{step}世代
+      </div>
+    );
   }
 }
 
-class textFieldRows extends React.Component {
-  render(){
-    return()
+class TextFieldRows extends React.Component {
+  render() {
+    return (
+      <div className="textFieldRows">
+        <input
+          type="number"
+          value={rowsNum}
+          onChange={handleRowsNumChange}
+          step="5"
+          min="5"
+          placeholder="辺のマス目を入力"
+        />
+      </div>
+    );
   }
 }
 
-class textFieldInitRate extends React.Component {
-  render(){
-    return()
-  }
+function TextFieldInitRate() {
+  return (
+    <div className="textFieldInitRate">
+      <input
+        type="number"
+        step="10"
+        max="100"
+        min="0"
+        placeholder="初期生命の割合を入力"
+      />
+    </div>
+  );
 }
 
 function NextStepButton() {
-    return(
-      <button className="nextStepButton" style={nextStepButtonStyle} onClick={nextState}>
-        進める
-      </button>
-    )
+  return (
+    <button
+      className="nextStepButton"
+      style={nextStepButtonStyle}
+      onClick={nextState}
+    >
+      進める
+    </button>
+  );
 }
 
 function AutoStepButton() {
-  return(
-    <button className="autoStepButton" onClick={auto} style={autoStepButtonStyle}>
-    オート
+  return (
+    <button
+      className="autoStepButton"
+      onClick={auto}
+      style={autoStepButtonStyle}
+    >
+      オート
     </button>
-  )
+  );
 }
 
 class LifeGame extends React.Component {
   render() {
     return (
       <div>
-        {/* <div className="lifeGameBoard" style={lifeGameBoardStyle}>
-          {createLifeGameBoard(step)}
-        </div> */}
-        <createLifeGameBoard step = step />
+        <CreateLifeGameBoard />
         <div>
-          {/* <div className="stepNum" style={stepNumStyle}>
-            第{step}世代
-          </div> */}
-          <stepNum step = step />
-          {/* <div className="textFieldRows">
-            <input
-              type="number"
-              value={rowsNum}
-              onChange={handleRowsNumChange}
-              step="5"
-              min="5"
-              placeholder="辺のマス目を入力"
-            ></input>
-          </div> */}
-          <textFieldRows rowsNum = rowsNum />
-          {/* <div className="textFieldInitRate">
-            <input
-              type="number"
-              step="10"
-              max="100"
-              min="0"
-              placeholder="初期生命の割合を入力"
-            ></input>
-          </div> */}
-          <textFieldInitRate />
+          <StepNum step={step} />
+          <TextFieldRows rowsNum={rowsNum} />
+          <TextFieldInitRate />
           <NextStepButton />
           <AutoStepButton />
         </div>
@@ -101,7 +199,7 @@ class LifeGame extends React.Component {
   }
 }
 
-// TODO: CSSを後で分離する;
+// CSS
 const lifeGameStyle = {
   display: "flex",
 };
@@ -119,7 +217,6 @@ const autoStepButtonStyle = {
   padding: "10px",
   margin: "10px",
 };
-
 
 // 以下コメントアウト
 // import { useState } from "react";
