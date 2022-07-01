edb565dbadb33253e4b4fd5d38ce971cdb6952f3
Kei Murakami <buncho514@icloud.com>
2022-07-01 19:19:09 +0900

    改善

--- src/pages/WantToBuy.tsx
+++ src/pages/WantToBuy.tsx
@@ -1,42 +1,37 @@
 import { useState } from "react";
 
+interface wantToBuyProps {
+  id: number;
+  item: string;
+}
 const WantToBuy = () => {
   const [idCounter, setIdCounter] = useState<number>(0);
-  const [wantToBuyList, setWantToBuy] = useState<object[]>([]);
+  const [wantToBuyItem, setWantToBuyItem] = useState<string>("");
+  const [wantToBuyList, setWantToBuyList] = useState<wantToBuyProps[]>([]);
 
-  const addWantToBuy = (
-    e:
-      | React.FormEvent<HTMLFormElement>
-      | {
-          preventDefault: () => void;
-          target: {
-            [x: string]: { value: string };
-          };
-        }
-  ) => {
-    e.preventDefault();
-    if ("wantToBuy" in e.target) {
-      if (!e.target["wantToBuy"].value) return;
-      const inputText: string = e.target["wantToBuy"].value;
-      const nextid: number = idCounter + 1;
-      setIdCounter(nextid);
-      setWantToBuy([...wantToBuyList, { id: nextid, item: inputText }]);
-    }
+  const addWantToBuy = () => {
+    const nextid: number = idCounter + 1;
+    setIdCounter(nextid);
+    setWantToBuyList([...wantToBuyList, { id: nextid, item: wantToBuyItem }]);
+    setWantToBuyItem("");
   };
 
   return (
-    <div className="WantToBuy">
-      ここに買いたい物リストを作るぞ.
-      <form onSubmit={addWantToBuy}>
-        <input name="wantToBuy" />
-        <button>買いたい物を追加</button>
-      </form>
+    <div>
+      ここに買いたい物リストを作るぞ。
+      <br />
+      <input
+        type="text"
+        value={wantToBuyItem}
+        onChange={(e) => setWantToBuyItem(e.target.value)}
+      />
+      <button onClick={addWantToBuy}>買いたい物を追加</button>
       <hr />
       <div>ここにリストを表示するぞ</div>
       <div>
-        {wantToBuyList.map((wantToBuy: any) => (
-          <li key={wantToBuy.id}>
-            {wantToBuy.id} : {wantToBuy.item}
+        {wantToBuyList.map((wantToBuyItem: wantToBuyProps) => (
+          <li key={wantToBuyItem.id}>
+            {wantToBuyItem.id} : {wantToBuyItem.item}
           </li>
         ))}
       </div>
