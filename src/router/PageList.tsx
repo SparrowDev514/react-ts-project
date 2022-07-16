7ebb22122698c9714591b0447b71f2d68d220e7c
Kei Murakami <buncho514@icloud.com>
2022-07-17 01:07:42 +0900

    README更新

--- src/router/PageList.tsx
+++ src/router/PageList.tsx
@@ -2,25 +2,28 @@ import { Link } from "react-router-dom";
 
 const PageList = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/wantToBuy">WantToBuy</Link>
-        </li>
-        <li>
-          <Link to="/commitCounter">CommitCounter</Link>
-        </li>
-        <li>
-          <Link to="/twitterManagement">TwitterManagement</Link>
-        </li>
-        <li>
-          <Link to="/lifeGame">LifeGame</Link>
-        </li>
-        <li>
-          <Link to="/cardGameMemo">CardGameMemo</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/wantToBuy">WantToBuy</Link>
+          </li>
+          <li>
+            <Link to="/commitCounter">CommitCounter</Link>
+          </li>
+          <li>
+            <Link to="/twitterManagement">TwitterManagement</Link>
+          </li>
+          <li>
+            <Link to="/lifeGame">LifeGame</Link>
+          </li>
+          <li>
+            <Link to="/cardGameMemo">CardGameMemo</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+    </>
   );
 };
 export default PageList;
