e7d3ff20a0a36b44df2b5f4b731f50f5fbd171bc
Kei Murakami <buncho514@icloud.com>
2022-07-17 09:36:18 +0900

    削除など

--- src/pages/WantToBuy.tsx
+++ src/pages/WantToBuy.tsx
@@ -8,12 +8,24 @@ const WantToBuy = () => {
   const [idCounter, setIdCounter] = useState<number>(0);
   const [wantToBuyItem, setWantToBuyItem] = useState<string>("");
   const [wantToBuyList, setWantToBuyList] = useState<wantToBuyProps[]>([]);
+  const [isActive, setIsActive] = useState<boolean>(false);
 
   const addWantToBuy = () => {
     const nextid: number = idCounter + 1;
     setIdCounter(nextid);
     setWantToBuyList([...wantToBuyList, { id: nextid, item: wantToBuyItem }]);
     setWantToBuyItem("");
+    setIsActive(false);
+  };
+
+  const removeItem = (id: number) => {
+    const newWantToBuyList = wantToBuyList.filter((item) => item.id != id);
+    setWantToBuyList([...newWantToBuyList]);
+  };
+
+  const validateTextField = (text: string) => {
+    setWantToBuyItem(text);
+    text ? setIsActive(true) : setIsActive(false);
   };
 
   return (
@@ -23,15 +35,22 @@ const WantToBuy = () => {
       <input
         type="text"
         value={wantToBuyItem}
-        onChange={(e) => setWantToBuyItem(e.target.value)}
+        onChange={(e) => {
+          validateTextField(e.target.value);
+        }}
       />
-      <button onClick={addWantToBuy}>買いたい物を追加</button>
+      <button disabled={!isActive} onClick={addWantToBuy}>
+        買いたい物を追加
+      </button>
       <hr />
       <div>ここにリストを表示するぞ</div>
       <div>
         {wantToBuyList.map((wantToBuyItem: wantToBuyProps) => (
           <li key={wantToBuyItem.id}>
             {wantToBuyItem.id} : {wantToBuyItem.item}
+            <button onClick={() => removeItem(wantToBuyItem.id)}>
+              削除する
+            </button>
           </li>
         ))}
       </div>
